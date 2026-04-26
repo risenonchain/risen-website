6cc7d662a3c69b1444b056f84deb386f6dbf73bc
gahtongo <beraccahjosh@gmail.com>
2026-04-26 01:59:28 +0100

    ads incluson

--- app/rush/layout.tsx
+++ app/rush/layout.tsx
@@ -1,6 +1,18 @@
 import "../globals.css";
 import SimpleBackToSiteLayout from "@/components/SimpleBackToSiteLayout";
+import Head from "next/head";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <SimpleBackToSiteLayout>{children}</SimpleBackToSiteLayout>;
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5182951866830006"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <SimpleBackToSiteLayout>{children}</SimpleBackToSiteLayout>
+    </>
+  );
 }
